472cc7d71832a68c0017bb8a51d3835324c41fc5
Jens Vanhee <jens.vanhee@student.hogent.be>
2021-04-18 22:44:39 +0200

    link FXML elements with KlantBeheerSchermController

--- src/gui/KlantBeheerSchermController.java
+++ src/gui/KlantBeheerSchermController.java
@@ -8,12 +8,90 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class KlantBeheerSchermController extends AnchorPane{
 	private AanmeldController adc;
+	
+	// Header (bovenaan)
+	@FXML private Button btnUitloggen;
+	@FXML private Label lblTitel;
+	@FXML private Label lblBegroeting;
+		
+	// Navigatie (links)
+	@FXML private Button btnHoofdmenu;
+	@FXML private Label lblGebruikerBeheer;
+	@FXML private Button btnWerknemerBeheer;
+	@FXML private Button btnKlantBeheer;
+	@FXML private Label lblTicketBeheer;
+	@FXML private Button btnTicket;
+	@FXML private Button btnTicketType;
+	@FXML private Button btnRapport;
+	@FXML private Label lblContractBeheer;
+	@FXML private Button btnContract;
+	@FXML private Button btnContractType;
+	@FXML private Label lblTaalWijzigen;
+	@FXML private ComboBox<String> cboTaalWijzigen;
+	
+	// Filters (midden)
+	@FXML private Label lblFilters;
+	@FXML private CheckBox chkActieveKlanten;
+	@FXML private CheckBox chkInactieveKlanten;
+	@FXML private CheckBox chkGeblokkeerdeKlanten;
+	@FXML private TextField txfFilterGebruikersnaam;
+	@FXML private TextField txfFilterVoornaam;
+	@FXML private TextField txfFilterNaam;
+	@FXML private TextField txfFilterBedrijf;
+	@FXML private Button btnClearFilters;
+	
+	// Klant tabel (midden)
+	@FXML private TableColumn tbcKlantsnr;
+	@FXML private TableColumn tbcGebruikersnaam;
+	@FXML private TableColumn tbcVoornaam;
+	@FXML private TableColumn tbcNaam;
+	@FXML private TableColumn tbcBedrijf;
+	@FXML private TableColumn tbcStatus;
+	
+	// Klant detail paneel (rechts)
+	@FXML private Label lblKlantgegevens;
+	@FXML private Label lblGebruikersnaam;
+	@FXML private TextField txfGebruikersnaam;
+	@FXML private Label lblVoornaam;
+	@FXML private TextField txfVoornaam;
+	@FXML private Label lblNaam;
+	@FXML private TextField txfNaam;
+	@FXML private Label lblEmail;
+	@FXML private TextField txfEmail;
+	@FXML private Label lblStatus;
+	@FXML private CheckBox chkStatus;
+	@FXML private Label lblBedrijfsgegevens;
+	@FXML private Label lblBedrijfsnaam;
+	@FXML private TextField txfBedrijfsnaam;
+	@FXML private Label lblLand;
+	@FXML private TextField txfLand;
+	@FXML private Label lblGemeente;
+	@FXML private TextField txfGemeente;
+	@FXML private Label lblStraat;
+	@FXML private TextField txfStraat;
+	@FXML private Label lblTelefoonnummers;
+	@FXML private TextArea txaTelefoonnummers;
+	@FXML private Label lblHuisnr;
+	@FXML private TextField txfHuisnr;
+	@FXML private Label lblBusnr;
+	@FXML private TextField txfBusnr;
+		
+	@FXML private Button btnKlantWijzigen;
+	@FXML private Button btnKlantToevoegen;
+	
 	public KlantBeheerSchermController(AanmeldController aanmeldController) {
 		this.adc = aanmeldController;
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("KlantBeheerScherm.fxml"));
@@ -27,6 +105,7 @@ public class KlantBeheerSchermController extends AnchorPane{
 	    }
 		
 	}
+	
 	@FXML
     void KlantBeheren(ActionEvent event) throws SQLException, IOException {
 		Stage stage = (Stage) this.getScene().getWindow();
@@ -41,6 +120,7 @@ public class KlantBeheerSchermController extends AnchorPane{
         });
         stage.show();
     }
+	
 	@FXML
     void WerknemerBeheren(ActionEvent event) throws SQLException, IOException {
 		Stage stage = (Stage) this.getScene().getWindow();
@@ -55,6 +135,7 @@ public class KlantBeheerSchermController extends AnchorPane{
         });
         stage.show();
     }
+	
 	@FXML
     void Hoofdmenu(ActionEvent event) throws SQLException, IOException {
 		Stage stage = (Stage) this.getScene().getWindow();
