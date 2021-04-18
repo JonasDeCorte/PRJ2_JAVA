a4908722424154f70da0a1eebee6965992af579f
Jens Vanhee <jens.vanhee@student.hogent.be>
2021-04-18 18:33:16 +0200

    link @FXML elements with WerknemerBeheerController

--- src/gui/WerknemerBeheerSchermController.java
+++ src/gui/WerknemerBeheerSchermController.java
@@ -4,16 +4,94 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import domein.controllers.AanmeldController;
+import domein.enumerations.WERKNEMERROL;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class WerknemerBeheerSchermController extends AnchorPane{
 	private AanmeldController adc;
+	
+	// Header (bovenaan)
+	@FXML private Button btnUitloggen;
+	@FXML private Label lblTitel;
+	@FXML private Label lblBegroeting;
+	
+	// Navigatie (links)
+	@FXML private Button btnHoofdmenu;
+	@FXML private Label lblGebruikerBeheer;
+	@FXML private Button btnWerknemerBeheer;
+	@FXML private Button btnKlantBeheer;
+	@FXML private Label lblTicketBeheer;
+	@FXML private Button btnTicket;
+	@FXML private Button btnTicketType;
+	@FXML private Button btnRapport;
+	@FXML private Label lblContractBeheer;
+	@FXML private Button lblContract;
+	@FXML private Button lblContractType;
+	@FXML private Label lblTaalWijzigen;
+	@FXML private ComboBox<String> cboTaalWijzigen;
+	
+	// Filters (midden)
+	@FXML private Label lblFilters;
+	@FXML private CheckBox chkActieveWerknemers;
+	@FXML private CheckBox chkInactieveWerknemers;
+	@FXML private CheckBox chkGeblokkeerdeWerknemers;
+	@FXML private TextField txfFilterGebruikersnaam;
+	@FXML private TextField txfFilterVoornaam;
+	@FXML private TextField txfFilterNaam;
+	@FXML private TextField txfFilterFunctie;
+	@FXML private Button btnClearFilters;
+	
+	// Werknemers tabel (midden)
+	@FXML private TableColumn tbcPersoneelsnr;
+	@FXML private TableColumn tbcGebruikersnaam;
+	@FXML private TableColumn tbcVoornaam;
+	@FXML private TableColumn tbcNaam;
+	@FXML private TableColumn tbcFunctie;
+	@FXML private TableColumn tbcStatus;
+	
+	// Werknemer detail paneel (rechts)
+	@FXML private Label lblPersonneelsgegevens;
+	@FXML private Label lblGebruikersnaam;
+	@FXML private TextField txfGebruikersnaam;
+	@FXML private Label lblVoornaam;
+	@FXML private TextField txfVoornaam;
+	@FXML private Label lblNaam;
+	@FXML private TextField txfNaam;
+	@FXML private Label lblEmail;
+	@FXML private TextField txfEmail;
+	@FXML private Label lblStatus;
+	@FXML private CheckBox chkStatus;
+	@FXML private Label lblFunctie;
+	@FXML private ComboBox<WERKNEMERROL> cboFunctie;
+	@FXML private Label lblAdresgegevens;
+	@FXML private Label lblLand;
+	@FXML private TextField txfLand;
+	@FXML private Label lblGemeente;
+	@FXML private TextField txfGemeente;
+	@FXML private Label lblStraat;
+	@FXML private TextField txfStraat;
+	@FXML private Label lblTelefoonnummers;
+	@FXML private TextField txfTelefoonnummers;
+	@FXML private Label lblHuisnr;
+	@FXML private TextField txfHuisnr;
+	@FXML private Label lblBusnr;
+	@FXML private TextField txfBusnr;
+		
+	@FXML private Button btnWerknemerWijzigen;
+	@FXML private Button btnWerknemerToevoegen;
+	
 	public WerknemerBeheerSchermController(AanmeldController aanmeldController) {
 		this.adc = aanmeldController;
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("WerknemerBeheerScherm.fxml"));
