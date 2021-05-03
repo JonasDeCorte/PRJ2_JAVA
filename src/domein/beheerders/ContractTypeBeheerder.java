6b326ee3593f59b9ff99235c4e1c263f1af22aec
Jonas De Corte <jonas.decorte@student.hogent.be>
2021-05-03 12:16:50 +0200

    filter contracttype & get all contracttypes

--- src/domein/beheerders/ContractTypeBeheerder.java
+++ src/domein/beheerders/ContractTypeBeheerder.java
@@ -1,12 +1,19 @@
 package domein.beheerders;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 import domein.ContractType;
 import domein.dao.ContractTypeDao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import repository.ContractTypeDaoJpa;
 
 public class ContractTypeBeheerder {
 	private ContractTypeDao contractTypeDao;
-
+	private FilteredList<ContractType> filteredContractTypeLijst;
 	private ContractTypeBeheerder(ContractTypeDao contractTypeDao) {
 		this.contractTypeDao = contractTypeDao;
 	}
@@ -15,6 +22,14 @@ public class ContractTypeBeheerder {
 		this(new ContractTypeDaoJpa());
 	}
 
+	public ObservableList<ContractType> haalKlantenOp() {
+		if (filteredContractTypeLijst == null) {
+			filteredContractTypeLijst = 
+					new FilteredList<>(FXCollections.observableArrayList(contractTypeDao.findAll()));
+								
+		}
+		return FXCollections.unmodifiableObservableList(filteredContractTypeLijst);
+	}
 	// toevoegen van een contract type
 	public void addContractType(ContractType contractType) {
 		if (!bestaatContractType(contractType.getNaam())) {
@@ -24,6 +39,7 @@ public class ContractTypeBeheerder {
 		} else {
 			throw new IllegalArgumentException("ContractType bestaat al.");
 		}
+		filteredContractTypeLijst = new FilteredList<>(FXCollections.observableArrayList(contractTypeDao.findAll()),filteredContractTypeLijst.getPredicate());
 
 	}
 
@@ -36,7 +52,7 @@ public class ContractTypeBeheerder {
 		} else {
 			throw new IllegalArgumentException("ContractType bestaat niet.");
 		}
-
+		filteredContractTypeLijst = new FilteredList<>(FXCollections.observableArrayList(contractTypeDao.findAll()),filteredContractTypeLijst.getPredicate());
 	}
 
 	// bestaat het contract type al?
@@ -47,5 +63,20 @@ public class ContractTypeBeheerder {
 	// get aantal actieve tickets die per contract type in behandeling zijn
 	public int getTicketsInBehandelingVanActiefContractType(String naam) {
 		return contractTypeDao.getTicketsInBehandelingVanActiefContractType(naam);
+	}
+	public void pasFilterAan(String naam, boolean statusActiefTrue, boolean StatusActiefFalse) {
+		// houdt de filters bij
+		List<Predicate<ContractType>> filtersLijst = new ArrayList<>();
+		if (naam != null && !naam.isBlank()) {
+			filtersLijst.add(ContractType -> ContractType.getNaam().toLowerCase().contains(naam.toLowerCase()));
+		}
+		if(statusActiefTrue) {
+			filtersLijst.add(ContractType -> ContractType.isStatus() == true);
+		}
+		if(StatusActiefFalse) {		
+			filtersLijst.add(ContractType -> ContractType.isStatus() == false);
+		}	
+		Predicate<ContractType> filterContractTypeLijst = filtersLijst.stream().reduce(Predicate::and).orElse(x -> true);
+		filteredContractTypeLijst.setPredicate(filterContractTypeLijst);
 	}
 }
