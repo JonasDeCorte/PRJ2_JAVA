e65ce8258ce7751d8792a1296edfe16b3aa4d796
SimonBekaertt <61085492+SimonBekaertt@users.noreply.github.com>
2021-05-18 23:23:42 +0200

    Update WerknemerTest.java

--- src/testen/WerknemerTest.java
+++ src/testen/WerknemerTest.java
@@ -3,6 +3,7 @@ package testen;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -17,25 +18,56 @@ import domein.Klant;
 import domein.Werknemer;
 import domein.enumerations.WERKNEMERROL;
 
-class WerknemerTest {
-	
-	  
-public Werknemer wn;
-	@Test
-	void new_WerknemerAanmakenMetLeegAdres_ThrowsException() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> new Werknemer("test", "test", "test", "test", "test@gmail.com", 5000,  new ArrayList<String>(), WERKNEMERROL.ADMINISTRATOR , null));
+
+public class WerknemerTest {
+	private static Stream<Arguments> foutieveParameters() {
+		return Stream.of(
+				
+				//foutief adres
+				Arguments.of("Simon", "wachtwoord", "simon", "bekaert", "simon.bekaert2000@hotmail.com",
+				1, Arrays.asList("+32472485464"), WERKNEMERROL.ADMINISTRATOR ,
+				null),
+				
+				//foutief werknemerrol
+				Arguments.of("Simon", "wachtwoord", "simon", "bekaert", "simon.bekaert2000@hotmail.com",
+						1, Arrays.asList("+32472485464"), null ,
+						new Adres("BE", "Zedelgem", "8210", "Guido Gezellelaan", 54, "10")),
+
+			
 	}
-	@Test
-	void new_WerknemerAanmakenMetLeegWerknemerrol_ThrowsException() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> new Werknemer("test", "test", "test", "test", "test@gmail.com", 5000,  new ArrayList<String>(), null, new Adres()));
+
+	private static Stream<Arguments> correcteParameters() {
+		return Stream.of(
+				Arguments.of("Simon", "wachtwoord", "simon", "bekaert", "simon.bekaert2000@hotmail.com",
+						1, Arrays.asList("+32472485464"), WERKNEMERROL.ADMINISTRATOR ,
+						new Adres("BE", "Zedelgem", "8210", "Guido Gezellelaan", 54, "10")));
 	}
+
 	@ParameterizedTest
-	 @EnumSource(value = WERKNEMERROL.class)
-	void new_WerknemerAanmakenMetWerknemerrol_Succes(WERKNEMERROL werknemerRol) {
-	 wn = 	new Werknemer("test", "test", "test", "test", "test@gmail.com", 5000,  new ArrayList<String>(), werknemerRol, new Adres());
-	assertNotNull(wn);
+	@MethodSource("correcteParameters")
+	public void correcteWerknemerAanmaken_steltAtributenCorrectIn(String gebruikersnaam, String wachtwoord, 
+			String voornaam, String naam, String emailadres, int personeelsnummer,  List<String> telefoonnummers, 
+			WERKNEMERROL werknemerRol , Adres adres) {
+		Werknemer werknemer = new Werknemer(gebruikersnaam, wachtwoord, voornaam, naam, emailadres, personeelsnummer, 
+				telefoonnummers, werknemerRol, adres);
 		
+		Assertions.assertEquals(gebruikersnaam, werknemer.getGebruikersnaam());
+		Assertions.assertEquals(wachtwoord, werknemer.getWachtwoord());
+		Assertions.assertEquals(voornaam, werknemer.getVoornaam());
+		Assertions.assertEquals(naam, werknemer.getNaam());
+		Assertions.assertEquals(emailadres, werknemer.getEmailadres());
+		Assertions.assertEquals(personeelsnummer, werknemer.getPersoneelsnummer());
+		Assertions.assertEquals(telefoonnummers, werknemer.getTelefoonnummers());
+		Assertions.assertEquals(adres, werknemer.getAdres());
 	}
 
-
+	@ParameterizedTest
+	@MethodSource("foutieveParameters")
+	public void foutieveWerknemerAanmaken_werptException(String gebruikersnaam, String wachtwoord, 
+			String voornaam, String naam, String emailadres, int personeelsnummer,  List<String> telefoonnummers, 
+			WERKNEMERROL werknemerRol , Adres adres) {
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> new Werknemer(gebruikersnaam, wachtwoord, voornaam, naam, emailadres, personeelsnummer, 
+						telefoonnummers, werknemerRol, adres));
+	}
 }
